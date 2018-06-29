9bfcaecb49a2d3400a03c11de77fb349798da22e
Michael Badri <badrimichael@gmail.com>
2018-06-28 22:43:00 -0400

    pollard.py: User input must be greater than 1. Stored result of gcd calculation.

--- pollard.py
+++ pollard.py
@@ -3,24 +3,29 @@
 
 from math import factorial, gcd
 
+
 # Algorithm calculates up to 10!.
 def pollard(n):
     for _ in range(1, 10):
-        if gcd(((2 ** factorial(_)) % n) - 1, n) != 1:
-            first_factor = gcd(((2 ** factorial(_)) % n) - 1, n)
+        result = gcd(((2 ** factorial(_)) % n) - 1, n)
+        if result != 1:
+            first_factor = result
             second_factor = n // first_factor
             if first_factor == n:
-               continue
+                continue
             print("Factor found via " + str(_) + "!")
             print(str(n) + " = " + str(first_factor) + " x " + str(second_factor))
             return
     print("Computation timed out. Your integer may be prime, too large, or p-1 may have a large prime factor.")
 
+
 print("Pollard's p-1 Factorization:\n" +
-"Note that the computation times out at 10! and this program is only meant to showcase the factorization algorithm.\n"
-+ "This is not a general-use factorization program. Source code available here: https://github.com/badrimichael/cryptography.")
+      "Note that the computation times out at 10! and this program "
+      "is only meant to showcase the factorization algorithm.\n"
+      + "This is not a general-use factorization program. "
+        "Source code available here: https://github.com/badrimichael/cryptography.")
 user_input = int(input("Enter the integer you would like to factor.\n"))
-if user_input <= 2:
-    print("Please enter an integer greater than 2.")
+if user_input <= 1:
+    print("Please enter an integer greater than 1.")
 else:
     pollard(user_input)
